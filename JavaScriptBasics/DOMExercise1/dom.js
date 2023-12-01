8881911ae1a7978dc48f2b93e92c31dee1267f2c
EJ-Lan <ervinjon_lanada@outlook.com>
2023-11-30 23:09:22 -0700

    Exercise 1 DOM

---
+++ JavaScriptBasics/DOMExercise1/dom.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const redText = document.createElement('p');
+redText.textContent = "Hey I'm red!";
+redText.style.color = "red";
+
+const blueText = document.createElement('h3');
+blueText.textContent = "I'm a blue h3!";
+blueText.style.color = "blue";
+
+container.appendChild(redText);
+container.appendChild(redText);
+
+const innerContainer = document.createElement('div');
+
+const innerDivHeading = document.createElement('h1');
+innerDivHeading.textContent = "I'm in a div";
+
+const innerDivP = document.createElement('p');
+innerDivP.textContent = "ME TOO!";
+
+innerContainer.appendChild(innerDivHeading);
+innerContainer.appendChild(innerDivP);
+
+container.appendChild(innerContainer);
+
+
